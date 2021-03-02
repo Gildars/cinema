4d92f899092ce023c72a43200aa8b53044abcf15
Pavel <thegilldars@gmail.com>
2021-03-03 01:11:32 +0200

    refactor(schema): updated sql schema

--- schema/cinema.sql
+++ schema/cinema.sql
@@ -1,74 +1,93 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Окт 05 2018 г., 07:33
--- Версия сервера: 5.7.21
--- Версия PHP: 7.1.16
+-- Host: 127.0.0.1
+-- Generation Time: Mar 03, 2021 at 12:05 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `cinema`
+-- Database: `cinema`
 --
+CREATE DATABASE IF NOT EXISTS `cinema` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cinema`;
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `actors`
+-- Table structure for table `actors`
 --
 
 DROP TABLE IF EXISTS `actors`;
-CREATE TABLE IF NOT EXISTS `actors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=347 DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `actors`
+(
+    `id`   int(11)     NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`),
+    KEY `name_2` (`name`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 3598
+  DEFAULT CHARSET = utf8;
 
+-- --------------------------------------------------------
 
 --
--- Структура таблицы `films`
+-- Table structure for table `films`
 --
 
 DROP TABLE IF EXISTS `films`;
-CREATE TABLE IF NOT EXISTS `films` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `release` smallint(4) NOT NULL,
-  `format` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=122 DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `films`
+(
+    `id`      int(11)      NOT NULL AUTO_INCREMENT,
+    `title`   varchar(255) NOT NULL,
+    `release` smallint(4)  NOT NULL,
+    `format`  varchar(10)  NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `title` (`title`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1027
+  DEFAULT CHARSET = utf8;
+
+-- --------------------------------------------------------
 
 --
--- Структура таблицы `films_actors`
+-- Table structure for table `films_actors`
 --
 
 DROP TABLE IF EXISTS `films_actors`;
-CREATE TABLE IF NOT EXISTS `films_actors` (
-  `id_film` int(11) NOT NULL,
-  `id_actor` int(11) NOT NULL,
-  KEY `FK_films_actors_films` (`id_film`),
-  KEY `FK_films_actors_actors` (`id_actor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+CREATE TABLE IF NOT EXISTS `films_actors`
+(
+    `id_film`  int(11) NOT NULL,
+    `id_actor` int(11) NOT NULL,
+    KEY `FK_films_actors_films` (`id_film`),
+    KEY `FK_films_actors_actors` (`id_actor`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 --
--- Ограничения внешнего ключа таблицы `films_actors`
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `films_actors`
 --
 ALTER TABLE `films_actors`
-  ADD CONSTRAINT `FK_films_actors_actors` FOREIGN KEY (`id_actor`) REFERENCES `actors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_films_actors_films` FOREIGN KEY (`id_film`) REFERENCES `films` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `FK_films_actors_actors` FOREIGN KEY (`id_actor`) REFERENCES `actors` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `FK_films_actors_films` FOREIGN KEY (`id_film`) REFERENCES `films` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
